5212dfbe40622be9c3879f8fe472153825da532d
CompanyProfile <alanood94895@gmail.com>
2026-04-06 14:42:52 +0400

    task18:makePi

---
+++ src/makePi.java
@@ -0,0 +1,11 @@
+public class makePi {
+    public static void main(String[] args) {
+        System.out.println(makePi()); // [3, 1, 4]
+    }
+
+    public static int[] makePi() {
+        return new int[]{3, 1, 4};
+    }
+
+
+}
