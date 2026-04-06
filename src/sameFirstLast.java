27560fff047e0e8d858477e1c79e47182a1a7d1a
CompanyProfile <alanood94895@gmail.com>
2026-04-06 14:10:17 +0400

    task10:sameFirstLast

---
+++ src/sameFirstLast.java
@@ -0,0 +1,12 @@
+public class sameFirstLast {
+    public static void main(String[] args) {
+        System.out.println(sameFirstLast(new int[]{1, 2, 3}));
+        System.out.println(sameFirstLast(new int[]{1, 2, 3, 1}));
+        System.out.println(sameFirstLast(new int[]{1, 2, 1}));
+    }
+
+    public static boolean sameFirstLast(int[] nums) {
+        return nums.length >= 1 && nums[0] == nums[nums.length - 1];
+    }
+
+}
