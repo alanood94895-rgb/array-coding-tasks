6c15fda5e137d08191767f52cb9245a3f4c4df4f
CompanyProfile <alanood94895@gmail.com>
2026-04-06 15:10:00 +0400

    task23:biggerTwo

---
+++ src/biggerTwo.java
@@ -0,0 +1,21 @@
+public class biggerTwo {
+
+        public static void main(String[] args) {
+            System.out.println(biggerTwo(new int[]{1, 2}, new int[]{3, 4}));
+            System.out.println(biggerTwo(new int[]{3, 4}, new int[]{1, 2}));
+            System.out.println(biggerTwo(new int[]{1, 1}, new int[]{1, 2}));
+        }
+
+        public static int[] biggerTwo(int[] a, int[] b) {
+            int sumA = a[0] + a[1];
+            int sumB = b[0] + b[1];
+
+            if (sumA >= sumB) {
+                return a;
+            } else {
+                return b;
+            }
+
+        }
+
+}
